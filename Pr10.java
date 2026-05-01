6f8fa259acb1a9cfcff46b70a83221cde4e5464d
rensitutorial <rensirafaliya@gmail.com>
2026-05-01 14:05:20 +0530

    Create Pr10.java

---
+++ Pr10.java
@@ -0,0 +1,31 @@
+public class pr10 {
+    public static int knapsack(int[] weights, int[] values, int capacity, int n) {
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {2, 3, 4, 5};
+        int[] values = {3, 4, 5, 6};
+        int capacity = 5;
+
+        int n = values.length;
+
+        int maxProfit = knapsack(weights, values, capacity, n);
+
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+}
